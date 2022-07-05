9bdffe101cbbdc27591f4c787882d2f68c450d09
Roberto <robertovagner04@gmail.com>
2022-07-04 22:48:00 -0300

    adicao de classes e implementacao api

---
+++ app/src/main/java/me/dio/simulator/domain/Match.kt
@@ -0,0 +1,18 @@
+package me.dio.simulator.domain
+
+import com.google.gson.annotations.SerializedName
+
+data class Match(
+
+        @SerializedName("descricao")
+        val descricao: String,
+        @SerializedName("local")
+        val place: Place,
+        @SerializedName("mandante")
+        val homeTeam: Team,
+        @SerializedName("visitante")
+        val awayTeam: Team
+
+)
+
+
